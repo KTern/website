878eaf65d84c699b064658be3aea7cf54d84da50
Nivedha1801 <vnivedha@kaartech.com>
2021-10-12 14:28:49 +0530

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,56 +19,104 @@ module.exports = {
       secondary: '#EAEDF2',
       white:'#ffffff',
       project: {
-        primary:'#2666BA',
-        secondary: '#F2F4FA',
-        bg: '#E7EAEF',
-        50: '#EBF0FA',
-        100: '#E3ECFA',
-        200: '#D4E3FA',
-        300: '#B6D1F9',
-        400:'#79ADF8'
+
+          primary: '#2666BA',
+
+          secondary: '#F2F4FA',
+
+          bg: '#E7EAEF',
+
+          50: '#EBF0FA',
+
+          100: '#E3ECFA',
+
+          200: '#D4E3FA',
+
+          300: '#B6D1F9',
+
+          400: '#79ADF8',
+
         },
-       process: {
-        primary:'#281C87',
-        secondary: '#E0DDEE',
-       
-        50: '#EEEDF8',
-        100: '#DDDBF1',
-        200: '#BAB6E3',
-        300: '#6758EE',
-        400:'#2011A7'
+
+        process: {
+
+          primary: '#281C87',
+
+          secondary: '#E5E2F3',
+
+
+
+          50: '#EEEDF8',
+
+          100: '#DDDBF1',
+
+          200: '#BAB6E3',
+
+          300: '#6758EE',
+
+          400: '#2011A7',
+
         },
+
         maps: {
-        primary:'#F28900',
-        secondary: '#FFE2D6',
-       
-        50: '#FFE2D6',
-        100: '#FFD3C2',
-        200: '#FFA785',
-        300: '#FF9970',
-        400:'#FF8A5C'
+
+          primary: '#F28900',
+
+          secondary: '#FFF6EB',
+
+
+
+          50: '#FFE2D6',
+
+          100: '#FFD3C2',
+
+          200: '#FFA785',
+
+          300: '#FF9970',
+
+          400: '#FF8A5C',
+
         },
-         mines: {
-        primary:'#BA2926',
-        secondary: '#FDE3ED',
-       
-        50: '#FAF0F0',
-        100: '#EAC2C5',
-        200: '#DB959A',
-        300: '#CD838C',
-        400:'#B93F4A'
+
+        mines: {
+
+          primary: '#BA2926',
+
+          secondary: '#F3E4E2',
+
+
+
+          50: '#FAF0F0',
+
+          100: '#EAC2C5',
+
+          200: '#DB959A',
+
+          300: '#CD838C',
+
+          400: '#B93F4A',
+
         },
-         
-          labs: {
-        primary:'#1C8720',
-        secondary: '#EFFBF1',
-       
-        50: '#F0F9F3',
-        100: '#D7F1DD',
-        200: '#BEE7C8',
-        300: '#3CCD60',
-        400:'#2BA84A'
-      },
+
+        labs: {
+
+          primary: '#1C8720',
+
+          secondary: '#E2F3E2',
+
+
+
+          50: '#F0F9F3',
+
+          100: '#D7F1DD',
+
+          200: '#BEE7C8',
+
+          300: '#3CCD60',
+
+          400: '#2BA84A',
+
+        },
       gray: {
         50: '#E7EAEF',
         100: '#DDE0E5',
