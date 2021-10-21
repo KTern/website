367aa9460fcd15b2769c7c4bc13d62d730537168
Nivedha1801 <vnivedha@kaartech.com>
2021-10-21 22:22:19 +0530

    all responsive

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ export default function Error () {
     return (
         <section className="relative py-10 2xl:py-20 bg-black overflow-hidden h-screen">
           <div className="absolute top-0 right-0 -mr-60 md:-mt-112 "><Image src="/404/circle.svg" alt="" width={500} height={400}/></div>
-          <div className="absolute top-0 right-0 h-40 lg:h-80 lg:mr-44 lg:mt-24"><Image  src="/404/five-stars.svg" alt="" width={400} height={300}/></div>
+          <div className="hidden lg:block absolute top-0 right-0 h-40 lg:h-80 lg:mr-44 lg:mt-24"><Image  src="/404/five-stars.svg" alt="" width={400} height={300}/></div>
           <div className="hidden lg:block absolute top-0 left-0 h-64 -ml-10"><Image  src="/404/blue-double-circle.svg" alt="" width={400} height={300}/></div>
           <div className="absolute bottom-0 left-0 h-80"><Image  src="/404/half-double-circle.svg" alt="" width={400} height={300}/></div>
           <div className="relative mt-10  px-4 mx-auto">
@@ -15,9 +15,9 @@ export default function Error () {
               </a>
             </Link>
             <div className="mx-40 "><Image   src="/404/error-404.svg" alt="" width={400} height={300}/></div>
-            <div className="max-w-md mx-auto text-center">
-            <h2 className="mb-4 text-2xl text-white font-bold font-heading">Sorry, we can&apos;t find that page or something has gone wrong...</h2>
-            <Link  href="/"><a className="  inline-block py-3 px-10 bg-white hover:bg-gray-50 hover:text-black shadow text-lg text-black font-bold rounded-r-xl rounded-b-xl transition duration-200 custom-card-subheading">Return to Home</a></Link>
+            <div className="max-w-md mx-auto text-center ">
+            <h2 className="mb-4 text-2xl text-white font-bold font-heading ">Sorry, we can&apos;t find that page or something has gone wrong...</h2>
+            <Link  href="/"><a className="  inline-block py-3 px-10  bg-white hover:bg-gray-50 hover:text-black shadow text-lg text-black font-bold rounded-r-xl rounded-b-xl transition duration-200 custom-card-subheading">Return to Home</a></Link>
             </div>
           </div>
         </div>
