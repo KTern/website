f54937d772f109d2626ecc5e297a96e11307e7b9
Nivedha1801 <vnivedha@kaartech.com>
2021-11-08 18:57:07 +0530

    new

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,5 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-MyApp.getInitialProps = async () => {
- 
-  return {props:{ token: token} }
-}
+
 export default MyApp
