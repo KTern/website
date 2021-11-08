17588638d3ec4c92ec129fd84db7f05e6d12c965
Nivedha1801 <vnivedha@kaartech.com>
2021-11-08 15:20:21 +0530

    rendering check

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,5 @@ function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 // export default appWithTranslation(MyApp);
-MyApp.getInitialProps = async () => {
-  let token = await Authorization();
-  return { token: token }
-}
+
 export default MyApp;
