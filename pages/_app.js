4d58f4de60e16a5deba8248be09585f1306cb3a6
Nivedha1801 <vnivedha@kaartech.com>
2021-11-08 20:24:06 +0530

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+function MyApp ({ Component, pageProps }) {
+  const router = useRouter()
 
-function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return <Component {...pageProps} />
 }
 
