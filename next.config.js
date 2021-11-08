bf335c410e26d2990b8f24be1a1b7636f6ebc723
Nivedha1801 <vnivedha@kaartech.com>
2021-11-08 12:28:32 +0530

    strapi integration-index

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ module.exports = {
       {
         // matching all API routes
         source: "/api/:path*",
+        destination:"https://api.ktern.com/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
