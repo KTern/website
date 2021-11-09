91f9fbcab154f1f75ad5ace1ce9960bc7a4ef36b
Edson Frainlar <fedson@kaartech.com>
2021-11-09 13:27:52 +0530

    revoke back version

--- next.config.js
+++ next.config.js
@@ -7,24 +7,7 @@ module.exports = {
 		defaultLocale: 'en',
 	},
 	images: {
-		domains: [
-			'storage.googleapis.com',
-			'images.unsplash.com',
-			'logodix.com',
-			'ktern.com',
-			'tracking.g2crowd.com',
-			'a.storyblok.com',
-			'static.thenounproject.com',
-			'encrypted-tbn0.gstatic.com',
-			'cdn0.iconfinder.com',
-			'images.squarespace-cdn.com',
-			'cdn.devdojo.com',
-			'dummyimage.com',
-			'images.unsplash.com',
-			'cdn1.iconfinder.com',
-			'unsplash.com',
-			's.clipartkey.com',
-		],
+		domains: ['storage.googleapis.com'],
 	},
 
 	headers() {
