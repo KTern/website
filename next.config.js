beac3c9f8fcad16952635ba739b7bdd6f3d61d0f
Nivedha1801 <vnivedha@kaartech.com>
2021-11-10 15:35:41 +0530

    test 1

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ module.exports = {
     locales: [ 'en', 'de' ],
     defaultLocale:'en'
   },
-   images: {
+  images: {
+      path: '/_next/image/', 
       domains: ['storage.googleapis.com','logodix.com','ktern.com','tracking.g2crowd.com','a.storyblok.com','static.thenounproject.com','encrypted-tbn0.gstatic.com','cdn0.iconfinder.com','images.squarespace-cdn.com','cdn.devdojo.com','dummyimage.com','images.unsplash.com','cdn1.iconfinder.com','unsplash.com','s.clipartkey.com'],
   },
  
