f4d898882e2dd9f5427fb5d4bb7acb906db2e9f8
Nivedha1801 <vnivedha@kaartech.com>
2021-11-11 18:33:19 +0530

    zoho form

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -150,93 +150,19 @@ export default function Resources({ data }) {
         <section className='w-full lg:p-10 '>
           <div className='flex flex-wrap md:-mx-4 -mb-4 md:mb-0 px-2 md:px-10'>
             {/* <!-- On demand Filter Options--> */}
-            <div className='hidden md:block w-full md:w-1/5 md:px-4 mb-4 mt-10  md:mb-0'>
-              <form action='#' method='post' className=''>
-                <div className='mb-6'>
-                  <h1 className='text-2xl mt-2 mb-2 card-heading'>Streams</h1>
-                  <hr className='mb-2' />
-
-                  
-                  <input type='checkbox' id='vehicle1' name='vehicle1' value='Bike' />
-                  {data.StreamsFilter.map((dt) => (
-                    <label key="dt" className='card-subheading' htmlFor='vehicle1'>
-                      Digital Maps
-                    </label>
-                  ))}
-
-                  <br />
-                </div>
-              </form>
-              <form action='#' method='post' className=''>
-                <div className='mb-6'>
-                  <h1 className='text-2xl mt-2 mb-2 card-heading'>Role</h1>
-                  <hr className='mb-2' />
-                  <label>
-                    <input type='checkbox' name='field-name' value='example value' />
-                    <span className='ml-1 card-subheading'>Project Manager</span>
-                  </label>
-                </div>
-                <div className='mb-6'>
-                  <label>
-                    <input type='checkbox' name='field-name' value='example value' />
-                    <span className='ml-1 card-subheading'>Test Manager</span>
-                  </label>
-                </div>
-                <div className='mb-6'>
-                  <label>
-                    <input type='checkbox' name='field-name' value='example value' />
-                    <span className='ml-1 card-subheading'>Tester</span>
-                  </label>
-                </div>
-                <div className='mb-6'>
-                  <label>
-                    <input type='checkbox' name='field-name' value='example value' />
-                    <span className='ml-1 card-subheading'>CEO/CIO/CXO/CTO</span>
-                  </label>
-                </div>
-                <div className='mb-6'>
-                  <label>
-                    <input type='checkbox' name='field-name' value='example value' />
-                    <span className='ml-1 card-subheading'>Consultant</span>
-                  </label>
-                </div>
-              </form>
-            </div>
+           
             {/* <!-- On demand Webinar Card Display--> */}
 
-            <div className='w-full md:w-4/5 md:pl-20 mb-4 md:mb-0  mt-10'>
+            <div className='w-full   md:pl-20 mb-4 md:mb-0  mt-10'>
               {/* Chips Section */}
-              <section className=' flex sm:pt-5  pb-10 items-center justify-end'>
+              {/* <section className=' flex sm:pt-5  pb-10 items-center justify-end'>
                 <input id='demo-2' style={{ width: "35%" }} type='search' placeholder='Search' />
-              </section>
+              </section> */}
               <section className='pb-10 '>
-                <Link href='/resources#ebooks'>
-                  <a className='hyperlink click-chip outline-green mr-3'>E-books</a>
-                </Link>
-                <Link href='/resources#white-papers'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>White paper</a>
-                </Link>
-                <Link href='/resources#customer-success-stories'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>Customer Success Story</a>
-                </Link>
-                <Link href='/resources#infographics'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>Infographics</a>
-                </Link>
-                <Link href='/resources#datasheets'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>Datasheets</a>
-                </Link>
-                <Link href='/resources#product-tours'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>Product tour</a>
-                </Link>
-                <Link href='/resources#analyst-reports'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>Analyst Report</a>
-                </Link>
-                <Link href='/resources#best-practice-guides'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>Best Practice guides</a>
-                </Link>
-                <Link href='/resources#presentations'>
-                  <a className=' hyperlink click-chip outline-green mr-3'>Presentations</a>
-                </Link>
+                {data.ResourceTypeFilters.map((dt)=>(<Link key="dt" href='/resources#ebooks'>
+                  <a className='hyperlink click-chip outline-green mr-3'>{data.FilterName}</a>
+                </Link>))}
+                
               </section>
               <div className='xl:gap-6 gap-3 grid sm:grid-cols-1 w-full grid-cols-2 xl:grid-cols-3 p-3 mx-auto xl:p-6'>
                 {/* {data.map(resource=>(
