a059170dacd5a03e4c1b809bc037063678291efa
Edson Frainlar <fedson@kaartech.com>
2022-03-18 01:53:59 +0530

    Added: Local Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		fontFamily: {
-			body: ['myriad-pro', ' sans-serif', 'ibm-plex'],
+			body: ['myriad-pro', ' sans-serif'],
 		},
 		extend: {
 			screens: {
