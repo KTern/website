9a201dee7548d7a793e920580c60f98db08e5c1f
Mugeshbabu <56474500+MUGESHBABU-MAP@users.noreply.github.com>
2023-06-16 14:43:57 +0530

    feature change

--- pages/index.js
+++ pages/index.js
@@ -171,7 +171,7 @@ function Home({ data, h_data, f_data }) {
             pageSEODet={data.pageSEO}
             // background={background}
             // eslint-disable-next-line react/jsx-boolean-value
-            feature={false}
+            feature={true}
             index={true}
           />
           </section>
