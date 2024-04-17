84957f8edad935f57a19b99f00ff1866e6886a0d
Edson Frainlar <fedson@kaartech.com>
2024-04-18 03:15:23 +0530

    next js 11 to 14

--- pages/schedule-demo.js
+++ pages/schedule-demo.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
 import Layout from "../component/Layout";
-import { InlineWidget } from "react-calendly";
+// import { InlineWidget } from "react-calendly";
 export default function ScheduleDemo({ h_data, f_data }) {
   return (
     <>
       <Layout h_data={h_data} f_data={f_data}>
-        <InlineWidget
+        {/* <InlineWidget
           styles={{ height: "850px", marginTop: "50px" }}
           url="https://calendly.com/ktern-ai/30min?hide_landing_page_details=1&hide_gdpr_banner=1&text_color=111217&primary_color=1b465f"
-        />
+        /> */}
       </Layout>
     </>
   );
