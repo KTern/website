40102149c77609882ada4b67a6794fc21f8f74e6
Edson Frainlar <fedson@kaartech.com>
2024-04-18 03:40:37 +0530

    next js 11 to 14

--- pages/resources/videos.js
+++ pages/resources/videos.js
@@ -3,76 +3,76 @@ import React from "react";
 // import useDynTabs from "react-dyn-tabs";
 
 export default function Videos({ h_data, f_data, data }) {
-  let Tabs = [];
-  data.tabs.forEach((ele) => {
-    let tab = {};
-    let subtabs = [];
-    tab["id"] = ele.tabId;
-    tab["title"] = ele.tabName;
-    ele.subTabs.forEach((ele1) => {
-      let videoList = [];
-      let subtab = {};
-      subtab["id"] = ele1.tabId;
-      subtab["title"] = ele1.tabName;
-      subtab["closable"] = false;
-      data.videosList.forEach((video) => {
-        if (video.tabId == ele.tabId && video.subTabId == ele1.tabId) {
-          videoList = video.videos;
-        }
-      });
-      // console.log(videoList[0])
-      // console.log("Videos",ele.tabName,ele1.tabName,videoList)
-      subtab["panelComponent"] = (porps) => {
-        return (
-          <div>
-            {videoList.map((dt) => (
-              <p key="dt">{dt.title}</p>
-            ))}
-          </div>
-        );
-      };
-      subtabs.push(subtab);
-    });
-    tab["panelComponent"] = (porps) => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const [TabList2, PanelList2, ready2] = useDynTabs({
-        tabs: subtabs,
-        selectedTabID: subtabs[0].id,
-      });
-      return (
-        <>
-          <TabList2></TabList2>
-          <PanelList2></PanelList2>
-        </>
-      );
-    };
-    tab["closable"] = false;
-    Tabs.push(tab);
-  });
-  // console.log("Tbas",Tabs[0].panelComponent);
+  // let Tabs = [];
+  // data.tabs.forEach((ele) => {
+  //   let tab = {};
+  //   let subtabs = [];
+  //   tab["id"] = ele.tabId;
+  //   tab["title"] = ele.tabName;
+  //   ele.subTabs.forEach((ele1) => {
+  //     let videoList = [];
+  //     let subtab = {};
+  //     subtab["id"] = ele1.tabId;
+  //     subtab["title"] = ele1.tabName;
+  //     subtab["closable"] = false;
+  //     data.videosList.forEach((video) => {
+  //       if (video.tabId == ele.tabId && video.subTabId == ele1.tabId) {
+  //         videoList = video.videos;
+  //       }
+  //     });
+  //     // console.log(videoList[0])
+  //     // console.log("Videos",ele.tabName,ele1.tabName,videoList)
+  //     subtab["panelComponent"] = (porps) => {
+  //       return (
+  //         <div>
+  //           {videoList.map((dt) => (
+  //             <p key="dt">{dt.title}</p>
+  //           ))}
+  //         </div>
+  //       );
+  //     };
+  //     subtabs.push(subtab);
+  //   });
+  //   tab["panelComponent"] = (porps) => {
+  //     // eslint-disable-next-line react-hooks/rules-of-hooks
+  //     const [TabList2, PanelList2, ready2] = useDynTabs({
+  //       tabs: subtabs,
+  //       selectedTabID: subtabs[0].id,
+  //     });
+  //     return (
+  //       <>
+  //         <TabList2></TabList2>
+  //         <PanelList2></PanelList2>
+  //       </>
+  //     );
+  //   };
+  //   tab["closable"] = false;
+  //   Tabs.push(tab);
+  // });
+  // // console.log("Tbas",Tabs[0].panelComponent);
 
-  const [TabList1, PanelList1, ready1] = useDynTabs({
-    tabs: Tabs,
-    selectedTabID: data.selectedTab,
-  });
-  return (
-    <Layout h_data={h_data} f_data={f_data}>
-      <div className="bg-bg">
-        <section className="py-10  flex flex-col items-center justify-center">
-          <h2 className="section-heading mb-4">{data.filterSection.heading}</h2>
-          <p className="section-subheading text-gray-10">
-            {data.filterSection.subheading}
-          </p>
-        </section>
-        <section className="py-5 px-20 mx-auto flex flex-col items-center justify-center">
-          <TabList1></TabList1>
-          <div className="px-20">
-            <PanelList1></PanelList1>
-          </div>
-        </section>
-      </div>
-    </Layout>
-  );
+  // const [TabList1, PanelList1, ready1] = useDynTabs({
+  //   tabs: Tabs,
+  //   selectedTabID: data.selectedTab,
+  // });
+  // return (
+  //   <Layout h_data={h_data} f_data={f_data}>
+  //     <div className="bg-bg">
+  //       <section className="py-10  flex flex-col items-center justify-center">
+  //         <h2 className="section-heading mb-4">{data.filterSection.heading}</h2>
+  //         <p className="section-subheading text-gray-10">
+  //           {data.filterSection.subheading}
+  //         </p>
+  //       </section>
+  //       <section className="py-5 px-20 mx-auto flex flex-col items-center justify-center">
+  //         <TabList1></TabList1>
+  //         <div className="px-20">
+  //           <PanelList1></PanelList1>
+  //         </div>
+  //       </section>
+  //     </div>
+  //   </Layout>
+  // );
 }
 export const getStaticProps = async () => {
   const res = await fetch("https://strapi.ktern.com/videos-library", {
